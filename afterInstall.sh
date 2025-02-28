597db7f21970d7b5a68ae72f3302eb690d484b1f
Víctor Donola Ferreira <56196766+vdonoladev@users.noreply.github.com>
2025-02-28 11:07:38 -0300

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -7,53 +7,37 @@ echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
 echo "                      PÓS INSTALAÇÃO "
 echo "+-----------------------------------+"
 sleep 2
-Q
-# --- REQUISITOS --- #
-# Certifique-se de que isso esteja sendo executado como root
-declare -f VERIFY_ROOT
-function VERIFY_ROOT()
-{
-  uid=$(id -ur)
-  if [ "$uid" != "0" ]; then
-    echo "ERRO: este script deve ser executado como root!"
-    if [ -x /usr/bin/sudo ]; then
-      echo "Tente: sudo $0"
-    fi
-    exit 1
-  fi
-}
-
-# --- TESTES E REQUISITOS --- #
-declare -f VERIFY_NET
-function VERIFY_NET()
-{
-  if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
-    echo -e "${VERMELHO}[ERRO!] - O computador não tem conexão com a Internet. Verifique sua rede. ${SEM_COR}"
-    exit 1
-  else
-    echo -e "${VERDE}[INFORMAÇÃO] - Conexão com a Internet está funcionando normalmente. ${SEM_COR}"
-  fi
-}
 
 # --- FUNÇÕES --- #
 # Atualizando os repositórios e fazendo a atualização do sistema
 echo "Verificando atualizações do sistema ..."
 sleep 2
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
-apt update -y
+sudo apt update -y
 clear
   
 echo "Instalando atualizações do sistema ..."
 sleep 2
-apt dist-upgrade -y
+sudo apt dist-upgrade -y
 clear
   
 echo "Removendo pacotes desnecessários ..."
 sleep 2
-apt autoremove -y
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+sudo apt autoremove -y
 clear
 
+echo "Instalando Softwares Properties Common ..."
+sleep 2
+apt install software-properties-common -y
+sudo clear
 
-# --- APLICAÇÕES --- #
-echo
+echo "Instalando cURL ..."
+sleep 2
+sudo apt install curl -y
+
+echo "Baixando e Instalando o Google Chrome ..."
+sleep 2
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O googlechrome.deb
+sudo apt install -y ./googlechrome.deb
+sudo apt install -f -y
