eb0b9afcf8d29a12f2f3e4ff33adc0f998602f15
Víctor Donola Ferreira <56196766+vdonoladev@users.noreply.github.com>
2025-02-28 10:45:31 -0300

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,11 +1,14 @@
-# --- VARIÁVEIS --- #
-CHOICE=Softwares
-
-# --- CORES --- #
-VERMELHO='\e[1;91m'
-VERDE='\e[1;92m'
-SEM_COR='\e[0m'
-
+# --- LOGO --- #
+echo "  _   _ _                 _          "
+echo " | | | | |__  _   _ _ __ | |_ _   _  "
+echo " | | | | '_ \| | | | '_ \| __| | | | "
+echo " | |_| | |_) | |_| | | | | |_| |_| | "
+echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
+echo "                      PÓS INSTALAÇÃO "
+echo "+-----------------------------------+"
+sleep 2
+Q
+# --- REQUISITOS --- #
 # Certifique-se de que isso esteja sendo executado como root
 declare -f VERIFY_ROOT
 function VERIFY_ROOT()
@@ -34,4 +37,23 @@ function VERIFY_NET()
 
 # --- FUNÇÕES --- #
 # Atualizando os repositórios e fazendo a atualização do sistema
+echo "Verificando atualizações do sistema ..."
+sleep 2
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+apt update -y
+clear
+  
+echo "Instalando atualizações do sistema ..."
+sleep 2
+apt dist-upgrade -y
+clear
+  
+echo "Removendo pacotes desnecessários ..."
+sleep 2
+apt autoremove -y
+clear
 
+
+# --- APLICAÇÕES --- #
+echo
