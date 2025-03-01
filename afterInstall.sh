17d737595e4e795000f78d1fbe0cb5c333d2977b
Víctor Donola Ferreira <56196766+vdonoladev@users.noreply.github.com>
2025-03-01 10:00:37 -0300

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,43 +1,103 @@
+# --- VARIABLES --- #
+CHOICE=Softwares
+TIME=1
+
+# --- REQUIREMENTS --- #
+
+# make sure this is running as root
+declare -f VERIFY_ROOT
+function VERIFY_ROOT()
+{
+	uid=$(id -ur)
+	if [ "$uid" != "0" ]; then
+	        echo "[ERROR!] - This script must be run as root"
+		if [ -x /usr/bin/sudo ]; then
+			echo "try: sudo $0"
+		fi
+	        exit 1
+	fi
+}
+
+# --- TESTS --- #
+declare -f VERIFY_NET
+function VERIFY_NET()
+{
+  if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+    echo -e "[ERROR!] - The computer has no Internet connection. Check your network."
+    exit 1
+  else
+    echo -e "[INFORMATION] - Internet connection is working normally."
+  fi
+}
+
 # --- LOGO --- #
 echo "  _   _ _                 _          "
 echo " | | | | |__  _   _ _ __ | |_ _   _  "
 echo " | | | | '_ \| | | | '_ \| __| | | | "
 echo " | |_| | |_) | |_| | | | | |_| |_| | "
 echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
-echo "                      PÓS INSTALAÇÃO "
+echo "                   POST INSTALLATION "
 echo "+-----------------------------------+"
-sleep 2
+sleep $TIME
 
-# --- FUNÇÕES --- #
-# Atualizando os repositórios e fazendo a atualização do sistema
-echo "Verificando atualizações do sistema ..."
-sleep 2
-sudo apt update -y
-clear
+# --- SYSTEM UPDATE FUNCTION --- #
+
+echo " "
+echo "Welcome to Ubuntu Settings!"
+echo " "
+echo "Choose an option below to get started!
+
+1 - Check the system repository
+2 - Show system updates
+3 - Install system updates
+4 - Clean the system
+5 - Remove unnecessary packages
+0 - Exit"
+
+echo " "
+echo -n "Chosen option: "
+read option
+case $option in
+  1)
+    echo "Checking the system for updates ..."
+    sleep $TIME
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
+    apt update -y
+    ;;
   
-echo "Instalando atualizações do sistema ..."
-sleep 2
-sudo apt dist-upgrade -y
-clear
+  2)
+    echo "Showing system updates ..."
+    sleep $TIME
+    apt list --upgradable
+    ;;
   
-echo "Removendo pacotes desnecessários ..."
-sleep 2
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
-sudo apt autoremove -y
-clear
-
-echo "Instalando Softwares Properties Common ..."
-sleep 2
-apt install software-properties-common -y
-sudo clear
-
-echo "Instalando cURL ..."
-sleep 2
-sudo apt install curl -y
-
-echo "Baixando e Instalando o Google Chrome ..."
-sleep 2
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O googlechrome.deb
-sudo apt install -y ./googlechrome.deb
-sudo apt install -f -y
+  3)
+    echo "Installing system updates ..."
+    sleep $TIME
+    apt dist-upgrade -y
+  
+  4)
+    echo "Cleaning the system ..."
+    apt clean
+    apt autoclean
+    ;;
+  
+  5)
+    echo "Removing unnecessary packages from the system ..."
+    sleep $TIME
+    apt autoremove -y
+    ;;
+  
+  0)
+    echo "Exiting ..."
+    sleep $TIME
+    exit 0
+    ;;
+  
+  *)
+    echo "Invalid option, try again!"
+    ;;
+    
+esac
+done
