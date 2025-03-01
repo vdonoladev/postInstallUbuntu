f615821f3a415f6f0000b3a3b8bc4eb4e533eeaf
Víctor Donola Ferreira <56196766+vdonoladev@users.noreply.github.com>
2025-03-01 10:55:33 -0300

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,103 +1,151 @@
 # --- VARIABLES --- #
-CHOICE=Softwares
-TIME=1
 
-# --- REQUIREMENTS --- #
+set -e
 
-# make sure this is running as root
-declare -f VERIFY_ROOT
-function VERIFY_ROOT()
-{
-	uid=$(id -ur)
-	if [ "$uid" != "0" ]; then
-	        echo "[ERROR!] - This script must be run as root"
-		if [ -x /usr/bin/sudo ]; then
-			echo "try: sudo $0"
-		fi
-	        exit 1
+# --- URL's --- #
+
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+# --- DIRECTORIES AND FILES --- #
+
+DIRECTORY_DOWNLOADS="$HOME/Programs"
+
+# --- COLORS --- #
+
+VERMELHO="\e[1;91m"
+VERDE="\e[1;92m"
+WITHOUT_COLOR="\e[0m"
+
+# --- FUNCTIONS --- #
+
+# Updating repositories and performing system updates
+
+apt_update(){
+	sudo apt update && sudo apt dist-upgrade -y
+}
+
+# --- TESTS AND REQUIREMENTS --- #
+
+# Internet connecting?
+
+testing_internet(){
+	if ! ping -c 1 8.8.8.8 -1 &> /dev/null; then
+ 		echo -e "${VERMELHO}[ERROR!] - The computer has no Internet connection. Check your network.${SEM_COR}"
+		exit 1
+	else
+ 		echo -e "${VERDE}[INFORMATION!] - Internet connection is working normally.${SEM_COR}"
 	fi
 }
 
-# --- TESTS --- #
-declare -f VERIFY_NET
-function VERIFY_NET()
-{
-  if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
-    echo -e "[ERROR!] - The computer has no Internet connection. Check your network."
-    exit 1
-  else
-    echo -e "[INFORMATION] - Internet connection is working normally."
-  fi
+# Removing occasional apt locks
+
+lock_apt(){
+	sudo rm /var/lib/dpkg/lock-frontend
+ 	sudo rm /var/cache/apt/archives/lock
 }
 
-# --- LOGO --- #
-echo "  _   _ _                 _          "
-echo " | | | | |__  _   _ _ __ | |_ _   _  "
-echo " | | | | '_ \| | | | '_ \| __| | | | "
-echo " | |_| | |_) | |_| | | | | |_| |_| | "
-echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
-echo "                   POST INSTALLATION "
-echo "+-----------------------------------+"
-sleep $TIME
+# Adding/Confirming 32-bit Architecture
 
-# --- SYSTEM UPDATE FUNCTION --- #
+add_archi386(){
+	sudo dpkg --add-architecture i386
+}
 
-echo " "
-echo "Welcome to Ubuntu Settings!"
-echo " "
-echo "Choose an option below to get started!
+# Updating the repository
 
-1 - Check the system repository
-2 - Show system updates
-3 - Install system updates
-4 - Clean the system
-5 - Remove unnecessary packages
-0 - Exit"
+just_apt_update(){
+	sudo apt update -y
+}
 
-echo " "
-echo -n "Chosen option: "
-read option
-case $option in
-  1)
-    echo "Checking the system for updates ..."
-    sleep $TIME
-    sudo rm /var/lib/dpkg/lock-frontend
-    sudo rm /var/cache/apt/archives/lock
-    apt update -y
-    ;;
-  
-  2)
-    echo "Showing system updates ..."
-    sleep $TIME
-    apt list --upgradable
-    ;;
-  
-  3)
-    echo "Installing system updates ..."
-    sleep $TIME
-    apt dist-upgrade -y
-  
-  4)
-    echo "Cleaning the system ..."
-    apt clean
-    apt autoclean
-    ;;
-  
-  5)
-    echo "Removing unnecessary packages from the system ..."
-    sleep $TIME
-    apt autoremove -y
-    ;;
-  
-  0)
-    echo "Exiting ..."
-    sleep $TIME
-    exit 0
-    ;;
-  
-  *)
-    echo "Invalid option, try again!"
-    ;;
-    
-esac
-done
+# --- .DEB SOFTWARE TO INSTALL --- #
+
+PROGRAMS_TO_INSTALL=(
+	snapd
+	flatpak
+ 	gparted
+	timeshift
+ 	gufw
+	synaptic
+ 	vlc
+ 	gnome-sushi
+	code
+	git
+ 	wget
+	ubuntu-restricted-extras
+)
+
+# --- DOWNLOADING AND INSTALLING EXTERNAL PROGRAMS --- #
+
+install_debs(){
+	echo -e "${VERDE}[INFORMATION!] - Downloading .deb packages${SEM_COR}"
+
+ 	mkdir "$DIRECTORY_DOWNLOADS"
+	wget -c "$URL_GOOGLE_CHROME" -P "$DIRECTORY_DOWNLOADS"
+
+	# Installing .deb packages downloaded in the previous session
+	echo -e "${verde}[INFORMATION!] - Installing downloaded .deb packages.${SEM_COR}"
+ 	sudo dpkg -i $DIRECTORY_DOWNLOADS/*.deb
+
+ 	# --- INSTALL PROGRAMS IN APT --- #
+	echo -e "${VERDE}[INFORMATION!] - Installing apt packages from repository.${SEM_COR}"
+
+ 	for program_name in ${PROGRAMS_TO_INSTALL[@]}; do
+		if ! dpkg -l | grep -1 $program_name; then # Only install if not already installed
+			sudo apt install "$program_name" -y
+	 	else
+			echo "[INSTALLED!] - $program_name"
+		fi
+	done	
+}
+
+# --- INSTALLING FLATPAK PACKAGES --- #
+
+install_flatpaks(){
+	echo -e "${VERDE}[INFORMATION!] - Installing flatpak packages.${SEM_COR}"
+
+	flatpak install flathub org.gimp.GIMP -y
+ 	flatpak install flathub com.spotify.Client -y
+	flatpak install flathub com.bitwarden.desktop -y
+	flatpak install flathub org.telegram.desktop -y
+ 	flatpak install flathub org.gnome.Boxes -y
+	flatpak install flathub org.flameshot.Flameshot -y
+}
+
+# --- INSTALLING SNAP PACKAGES --- #
+
+install_snaps(){
+	echo -e "${VERDE}[INFORMATION!] - Installing snap packages.${SEM_COR}"
+
+	sudo snap install authy
+ 	sudo snap install libreoffice
+}
+
+# --- POST INSTALLATION --- #
+
+# Finalization, update and cleanup
+
+system_clean(){
+	apt_update -y
+ 	flatpak update -y
+	sudo apt autoclean -y
+ 	sudo apt autoremove -y
+ 	nautilus -q
+}
+
+# --- EXECUTION --- #
+
+lock_apt
+testing_internet
+lock_apt
+apt_update
+lock_apt
+add_archi386
+just_apt_update
+install_debs
+install_flatpaks
+install_snaps
+apt_update
+system_clean
+
+# --- FINALIZATION --- #
+
+echo -e "${VERDE}[INFORMATION!] - Script finished, installation complete! :)${SEM_COR}"
