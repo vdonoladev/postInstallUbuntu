ace29789bae6cbd1b7fbb82a913cc8eb3d1b2220
Víctor Donola Ferreira <56196766+vdonoladev@users.noreply.github.com>
2025-03-01 13:28:45 -0300

    Update systemUpdate.sh

--- systemUpdate.sh
+++ systemUpdate.sh
@@ -1,58 +1,72 @@
+# --- VARIABLES --- #
+
 TIME=1
-clear
-while true; do
-echo " "
-echo "Bem-vindos às configurações do Ubuntu!"
-echo " "
-echo "Escolha uma opção abaixo para começar!
 
-1 - Verificar o repositório do sistema
-2 - Mostrar as atualizações do sistema
-3 - Instalar as atualizações do sistema
-4 - Limpar o sistema
-5 - Remover os pacotes desnecessários do sistema
-0 - Sair"
+# --- LOGO --- #
+echo "  _   _ _                 _          "
+echo " | | | | |__  _   _ _ __ | |_ _   _  "
+echo " | | | | '_ \| | | | '_ \| __| | | | "
+echo " | |_| | |_) | |_| | | | | |_| |_| | "
+echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
+echo "                   POST INSTALLATION "
+echo "+-----------------------------------+"
+sleep $TIME
+
+# --- SYSTEM UPDATE FUNCTION --- #
+echo " "
+echo "Welcome to Ubuntu Settings!"
+echo " "
+echo "Chose an option below to get started!
+
+1 - Check the system repository
+2 - Show system updates
+3 - Install system updates
+4 - Clean the system
+5 - Remove unnecessary packages
+0 - Exit"
 
 echo " "
-echo -n "Opção escolhida: "
+echo -n "Chosen option: "
 read option
 case $option in
   1)
-    echo Verificando atualizações do sistema ...
+    echo "Checking the system for updates ..."
     sleep $TIME
-    apt update
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm/var/cache/apt/archives/lock
+    sudo apt update
     ;;
   
   2)
-    echo Mostrando atualizações do sistema ...
+    echo "Showing system updates ..."
     sleep $TIME
-    apt list --upgradable
+    sudo apt list --upgradable
     ;;
   
   3)
-    echo Instalando atualizações do sistema ...
+    echo "Installing system updates ..."
     sleep $TIME
-    apt upgrade -y
+    sudo apt upgrade -y
   
   4)
-    echo Limpando o sistema ...
-    apt clean
-    apt autoclean
+    echo "Cleaning the system ..."
+    sudo apt clean -y
+    sudo apt autoclean -y
     ;;
   
   5)
-    echo Removendo pacotes desnecessários do sistema ...
+    echo "Removing unnecessary packages from the system ..."
     sleep $TIME
     apt autoremove -y
     ;;
   
   0)
-    echo Saindo ...
+    echo "Exiting ..."
     sleep $TIME
     exit 0
   
   *)
-    echo Opção inválida, tente novamente!
+    echo "Opção inválida, tente novamente!"
     ;;
     
 esac
