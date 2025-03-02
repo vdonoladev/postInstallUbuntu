1bbd6531859aa06e2e08fc6e9ea5791a9220814b
vdonoladev <victordonolaf@gmail.com>
2025-03-02 17:09:17 -0300

    updating file

--- systemUpdate.sh
+++ systemUpdate.sh
@@ -10,7 +10,6 @@ echo " | |_| | |_) | |_| | | | | |_| |_| | "
 echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
 echo "                   POST INSTALLATION "
 echo "+-----------------------------------+"
-sleep $TIME
 
 # --- SYSTEM UPDATE FUNCTION --- #
 echo " "
@@ -29,46 +28,46 @@ echo " "
 echo -n "Chosen option: "
 read option
 case $option in
-  1)
-    echo "Checking the system for updates ..."
-    sleep $TIME
-    sudo rm /var/lib/dpkg/lock-frontend
-    sudo rm/var/cache/apt/archives/lock
-    sudo apt update
-    ;;
-  
-  2)
-    echo "Showing system updates ..."
-    sleep $TIME
-    sudo apt list --upgradable
-    ;;
-  
-  3)
-    echo "Installing system updates ..."
-    sleep $TIME
-    sudo apt upgrade -y
-    ;;
-  
-  4)
-    echo "Cleaning the system ..."
-    sudo apt clean -y
-    sudo apt autoclean -y
-    ;;
-  
-  5)
-    echo "Removing unnecessary packages from the system ..."
-    sleep $TIME
-    apt autoremove -y
-    ;;
-  
-  0)
-    echo "Exiting ..."
-    sleep $TIME
-    exit 0
-    ;;
-  
-  *)
-    echo "Opção inválida, tente novamente!"
-    ;;
-    
+1)
+  echo "Checking the system for updates ..."
+  sleep $TIME
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+  sudo apt update
+  ;;
+
+2)
+  echo "Showing system updates ..."
+  sleep $TIME
+  sudo apt list --upgradable
+  ;;
+
+3)
+  echo "Installing system updates ..."
+  sleep $TIME
+  sudo apt upgrade -y
+  ;;
+
+4)
+  echo "Cleaning the system ..."
+  sudo apt clean -y
+  sudo apt autoclean -y
+  ;;
+
+5)
+  echo "Removing unnecessary packages from the system ..."
+  sleep $TIME
+  apt autoremove -y
+  ;;
+
+0)
+  echo "Exiting ..."
+  sleep $TIME
+  exit 0
+  ;;
+
+*)
+  echo "Opção inválida, tente novamente!"
+  ;;
+
 esac
