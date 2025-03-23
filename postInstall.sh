2ac6d8063912a70179e69cf9ff7f4b0bbe139bf0
vdonoladev <victordonolaf@gmail.com>
2025-03-23 11:04:18 -0300

    changes in postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -8,11 +8,25 @@
 
 # --- VARIÁVEIS --- #
 set -e
+TIME=5
+
+# --- LOGO --- #
+echo "  _   _ _                 _          "
+echo " | | | | |__  _   _ _ __ | |_ _   _  "
+echo " | | | | '_ \| | | | '_ \| __| | | | "
+echo " | |_| | |_) | |_| | | | | |_| |_| | "
+echo "  \___/|_.__/ \__,_|_| |_|\__|\__,_| "
+echo "                   POST INSTALLATION "
+echo "+-----------------------------------+"
+
+sleep $TIME
 
 # --- TESTES E REQUISITOS --- #
 echo -e "---------------------"
 echo -e "Testes e Requisitos"
 echo -e "---------------------"
+
+sleep $TIME
 
 # Verificando se o usuário é root
 if [ $(id -u) -ne 0 ]; then
@@ -36,18 +50,12 @@ echo -e "Adicionando e confirmando arquitetura de 32 bits/i386..."
 
 dpkg --add-architecture i386
 
-# Criando diretório de downloads
-echo -e "Criando diretório de downloads..."
-
-DIR_DOWNLOADS="$HOME/Downloads/Programs"
-if [ ! -d $DIR_DOWNLOADS ]; then
-  mkdir $DIR_DOWNLOADS
-fi
-
 # --- CONFIGURAÇÕES --- #
 echo -e "------------------------"
 echo -e "Configurando o sistema"
 echo -e "------------------------"
+
+sleep $TIME
 
 # Minimizar janela ao clicar no ícone
 echo -e "Minimizando janela ao clicar no ícone..."
@@ -64,13 +72,13 @@ echo -e "-------------------------"
 echo -e "Instalando pacotes .DEB"
 echo -e "-------------------------"
 
+sleep $TIME
+
 echo -e "Instalando Google Chrome..."
-cd ~/Downloads/Programs
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
 echo -e "Instalando SimpleNote..."
-cd ~/Downloads/Programs
 wget -c https://github.com/Automattic/simplenote-electron/releases/download/v2.23.2/Simplenote-linux-2.23.2-amd64.deb
 dpkg -i Simplenote-linux-2.23.2-amd64.deb
 
@@ -79,14 +87,13 @@ echo -e "------------------------------"
 echo -e "Instalando programas via APT"
 echo -e "------------------------------"
 
+sleep $TIME
+
 echo -e "Instalando ubuntu-restricted-extras..."
 apt install ubuntu-restricted-extras -y
 
 echo -e "Instalando ubuntu-restricted-addons..."
 apt install ubuntu-restricted-addons -y
-
-echo -e "Instalando ubuntu-restricted-addons-common..."
-apt install ubuntu-restricted-addons-common -y
 
 echo -e "Instalando git..."
 apt install git -y
@@ -118,9 +125,6 @@ apt install snapd -y
 echo -e "Instalando apt-transport-https..."
 apt install apt-transport-https -y
 
-echo -e "Instalando code..."
-apt install code -y
-
 echo -e "Instalando ca-certificates..."
 apt install ca-certificates -y
 
@@ -129,9 +133,6 @@ apt install software-properties-common -y
 
 echo -e "Instalando gnome-shell-extensions..."
 apt install gnome-shell-extensions -y
-
-echo -e "Instalando gnome-suhi..."
-apt install gnome-suhi -y
 
 echo -e "Instalando gnome-shell-extension-manager..."
 apt install gnome-shell-extension-manager -y
@@ -152,9 +153,6 @@ echo -e "Instalando libreoffice..."
 apt install libreoffice -y
 apt install libreoffice-l10n-pt-br -y
 apt install libreoffice-style-breeze -y
-apt install libreoffice-style-oxygen -y
-apt install libreoffice-gtk -y
-apt install libreoffice-dde -y
 
 echo -e "Instalando pacote de fontes da Microsoft..."
 apt install msttcorefonts -y --force-yes
@@ -164,8 +162,6 @@ apt install transmission -y
 
 echo -e "Instalando meta-pacote de codecs..."
 apt install lame mpgtx sox -y
-apt install xubuntu-restricted-extras -y
-apt install mint-meta-codecs-core -y
 
 echo -e "Instalando Synaptic..."
 apt install synaptic -y
@@ -178,6 +174,8 @@ apt install apturl-common -y
 echo -e "----------------------------------"
 echo -e "Instalando programas via Flatpak"
 echo -e "----------------------------------"
+
+sleep $TIME
 
 echo -e "Instalando Discord..."
 flatpak install flathub com.discordapp.Discord -y
@@ -195,6 +193,8 @@ flatpak install flathub org.gnome.Boxes -y
 echo -e "------------------------------------------------------"
 echo -e "Finalizando, limpando e reinicializando o sistema..."
 echo -e "------------------------------------------------------"
+
+sleep $TIME
 
 dpkg --configure -a
 apt update && apt upgrade -y
